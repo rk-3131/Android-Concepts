3083e210cb24c2b38382dbfc3b2e14621b427854
Radhakrushna Mahadik <mahdikrs512@gmail.com>
2024-01-26 23:09:33 +0530

    Tic Tac Toe GUI added

---
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void onTap(View view){
+        Toast.makeText(this, "Touched", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
