768c55f058626c8296e9c1b8cb8bdec228d1ebde
Radhakrushna Mahadik <mahdikrs512@gmail.com>
2024-02-12 16:57:02 +0530

    Task scheduler with Runable and handler and countDownTimer added

---
+++ TaskHandler/app/src/main/java/com/example/taskhandler/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.taskhandler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int countInf = 1;
+    int countFin = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button finite = findViewById(R.id.buttonFinite);
+        Button infinite = findViewById(R.id.buttonInfinity);
+
+        Handler handler = new Handler();
+
+        infinite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Toast Number " + countInf, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        
+        finite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new CountDownTimer(5000, 1000){
+                    @Override
+                    public void onTick(long l) {
+                        Toast.makeText(MainActivity.this, "Toast Number " + countFin, Toast.LENGTH_SHORT).show();
+                        countFin++;
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        Toast.makeText(MainActivity.this, "Finite Toast has been finished", Toast.LENGTH_SHORT).show();
+                    }
+                }.start();
+            }
+        });
+    }
+}
